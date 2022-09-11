55b4e3f6c92ac969612787ccb5a13f9bfa3df78a
georgekasiyandima <georgekasiyandima@yahoo.com>
2022-09-11 13:48:38 +0200

    Added all dom

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -5,6 +5,18 @@ export default class App extends React.Component {
     return (
       <div>
         Todo App
+        <ul>
+          <li>Walk the dog</li>
+          <li>Take out trash</li>
+          <li>Buy groceries</li>
+        </ul>
+
+        <form>
+          <input/>
+          <button>Add</button>
+        </form>
+
+        <button>Clear</button>
       </div>
     )
   }
