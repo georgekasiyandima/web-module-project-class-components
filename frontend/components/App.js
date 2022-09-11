4449f04bc7ad381b7914ff06b1da02708e0f8d9b
georgekasiyandima <georgekasiyandima@yahoo.com>
2022-09-11 16:06:55 +0200

    Build from state and completed status

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,14 +1,34 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          name: 'Organize Garage',
+          id: 1528817077286,
+          completed: true
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: true
+        }
+      ]
+    }
+  }
   render() {
+
     return (
       <div>
         Todo App
         <ul>
-          <li>Walk the dog</li>
-          <li>Take out trash</li>
-          <li>Buy groceries</li>
+          {
+            todos.map(todo=> {
+              return (<li>{todo.task} { todo.completed?<span>-completed</span> : <span></span>}</li>)
+            })
+          }
         </ul>
 
         <form>
